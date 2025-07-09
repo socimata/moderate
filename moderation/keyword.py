d502b387bd15f3ac28ebd3164b92fe4951040a4c
Muspi Merol <me@promplate.dev>
2025-07-09 12:44:40 +0800

    deduplicate keyword detection results

--- moderation/keyword.py
+++ moderation/keyword.py
@@ -11,5 +11,14 @@ def keyword_map():
     return {i["keyword"]: i for i in keywords}
 
 
-def detect_keywords(text: str):
-    return [*filter(text.__contains__, keyword_map)]
+def detect_keywords(text: str, deduplicate=True):
+    results = [*filter(text.__contains__, keyword_map)]
+
+    if deduplicate:
+        for i, keyword in enumerate(reversed(s := sorted(results, reverse=True, key=len)), start=1):
+            for other in s[:-i]:
+                if keyword in other:
+                    results.remove(keyword)
+                    break
+
+    return results
